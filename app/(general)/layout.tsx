8f35b3e3fedd18a8a319410805a1609bb3b438c3
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-20 17:24:30 +0100

    feat: Update GeneralLayout component
    Update the GeneralLayout component to include a Navbar component at the top of the layout. This change ensures that the navigation bar is displayed on all pages that use the GeneralLayout. The Navbar component provides links to the Home, About, Pricing, and Contact pages.

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,12 +1,17 @@
-export default function AboutLayout({
+import { Navbar } from "@/components";
+
+export default function GeneralLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-5xl">Hola mundo...</span>
-      {children}
-    </main>
+    <>
+    <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span className="text-5xl">Hola mundo...</span>
+        {children}
+      </main>
+    </>
   );
 }
