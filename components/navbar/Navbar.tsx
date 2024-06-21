ea7351a4e5a81839a7ce5b636e55e2655b249a6c
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-21 10:08:21 +0100

    feat: Update Navbar component to use Next.js Link for navigation
    The Navbar component in the Navbar.tsx file has been updated to use Next.js Link component for navigation instead of anchor tags. This change ensures better performance and improves accessibility. The Navbar now includes links to the Home, About, Pricing, and Contact pages.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,13 +1,28 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  { path: "About", text: "/about" },
+  { path: "Pricing", text: "/pricing" },
+  { path: "Contact", text: "/contact" },
+];
+
 export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href="/" className="flex items-center">
+        <HomeIcon size={24} className="mr-2" />
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
 
-      <a className="mr-2" href="/about"> About </a>
-      <a className="mr-2" href="/pricing"> Pricing </a>
-      <a className="mr-2" href="/contact"> Contact </a>
+      {navItems.map((item) => (
+        <Link key={item.path} href={item.text} className="mr-2">
+          {item.path}
+        </Link>
+      ))}
+
     </nav>
   );
 };
